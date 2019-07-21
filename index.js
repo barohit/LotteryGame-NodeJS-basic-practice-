3c3a4a4ed989c7cfd0e1bbfca8f17a75da54c7eb
Rohit <rohit.r.baddam@gmail.com>
2019-07-21 15:12:47 -0400

    updated version, will add styling soon

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const app = express(); 
+const urlParserEncoder = bodyParser.urlencoded({extended: false});
 
 app.set('view engine', 'ejs'); 
+app.use(express.static(path.join(__dirname, 'public', 'assets')));
 
 app.listen(5000, (err) => {
     if (err) {
@@ -24,11 +27,54 @@ app.get("/ticket", (req, res) => {
     console.log("found ticket");
     let ticket = []; 
     for (let i = 0; i < 5; i++) {
-        const num = Math.random() * 75; 
+        const num = Math.random() * 70; 
+        if (num > 69.5) {
+            num = 70; 
+        }
         ticket.push(Math.floor(num)); 
     }
-    let mega = Math.random() * 20; 
+    let mega = Math.random() * 25; 
+    if (mega > 24.5) {
+        mega = 25;
+    }
     mega = Math.floor(mega);
     ticket.push(`Mega: ${mega}`); 
     res.render('ticket', {ticket: ticket}); 
+})
+
+app.post("/customticket", urlParserEncoder, (req, res) => {
+    let ticket = []; 
+    ticket.push(req.body.first); 
+    ticket.push(req.body.second);
+    ticket.push(req.body.third);
+    ticket.push(req.body.fourth);
+    ticket.push(req.body.fifth);
+    ticket.push(`Mega: ${req.body.mega}`);
+    res.render('ticket', {ticket:ticket});
+});
+
+app.post("/check", urlParserEncoder, (req, res) => {
+    let result = []; 
+    for (let i = 0; i < 5; i++) {
+        let num = Math.random() * 70; 
+        if (num > 69.5) {
+            num = 70; 
+        }
+        num = Math.floor(num); 
+        result.push(num);
+    }
+    let num = Math.random() * 25; 
+    if (num > 24.5) {
+        num = 25; 
+    } 
+    num = Math.floor(num); 
+    result.push(`Mega: ${num}`); 
+    let match = true; 
+    for (let i = 0; i < result.length; i++) {
+        if (result[i] !== req.body.ticket[i]) {
+            match = false; 
+        }
+    }
+    res.render('result', {result: result, match: match}); 
+
 })
---
+++ public/assets/validate.js
@@ -0,0 +1,19 @@
+function validate() {
+    const num1 = document.getElementsByName("first")[0].value;
+    const num2 = document.getElementsByName("second")[0].value;
+    const num3 = document.getElementsByName("third")[0].value;
+    const num4 = document.getElementsByName("fourth")[0].value;
+    const num5 = document.getElementsByName("fifth")[0].value;
+    const mega = document.getElementsByName("mega")[0].value;
+    if (!(num1 - Math.floor(num1) > 0.000001 || num2 - Math.floor(num2) > 0.000001 || num3 - Math.floor(num3) > 0.000001 || num4 - Math.floor(num4) > 0.000001 || num5 - Math.floor(num5) > 0.000001)) {
+        if ((num1 > 70 || num1 < 1) || (num2 > 70 || num2 < 1) || (num3 > 70 || num3 < 1) || (num4 > 70 || num4 < 1) || (num5 > 70 || num5 < 1) || (mega > 25 || mega < 1)) {
+            alert("Sorry, one or more of your numbers is out of range");
+            return false; 
+        } else {
+            return true; 
+        }
+    } else {
+        alert("Sorry, you must enter a whole number"); 
+        return false; 
+    }
+}
